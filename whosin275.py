e41336ab7912f14255d79904d47b5de8bc1d3d94
pparadigm <priparadigm@gmail.com>
2013-07-24 14:52:04 -0500

    implemented master key, greeting message

--- whosin275.py
+++ whosin275.py
@@ -1,6 +1,6 @@
 # written in Python 2.7.5 by Prime Paradigm (@pparadigm on GitHub)
 # developed on a Win7 system
-# last updated: July 24, 2013 @ 1:00PM
+# last updated: July 24, 2013 @ 2:47PM
 
 # This program is meant to handle the sign-in process with the RFID keys, and
 # allow management of the system.
@@ -11,10 +11,7 @@
 #
 # 1. Make it so RFIDKeyDict comes from a separate document
 # 2. Ability to have keys changed (added to system, deleted from system)
-# 3. Master key
-# 4. Greet people as they scan in, something like "Valid key.\nWelcome,
-#    Joseph."
-# 5. Ask for port name on first run, have options to save preferences
+# 3. Ask for port name on first run, have options to save preferences
 #
 # ---------
 
@@ -28,8 +25,7 @@ import serial
 RFIDKeyDict = {"06004FE1953D": "Joseph Livingdale",# 1 (see 'RFID Chart Info.txt')
                "06006F570B35": "Alice Varr",       # 2
                "06007266F7E5": "Grey Shultz",      # 3
-               "0600530B6C32": "Madilynne Fuller", # 4
-               "0600727FF0FB": "Sean Parmey"}      # 5
+               "0600727FF0FB": "MASTER"}           # 5
 count = 0
 listening = True
 
@@ -42,7 +38,13 @@ while listening:
     count = count + 1
     if count == 1:
         serIn = ser.readline()[1:13]
-        print serIn
+        if serIn in RFIDKeyDict:
+            print "Key recognized.\nWelcome, %s."%(RFIDKeyDict[serIn])
+        else:
+            print "Key not recognized."
     else:
         serIn = ser.readline()[2:14]
-        print serIn
+        if serIn in RFIDKeyDict:
+            print "Key recognized.\nWelcome, %s."%(RFIDKeyDict[serIn])
+        else:
+            print "Key not recognized."
