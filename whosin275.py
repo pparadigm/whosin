b14de5f1f8aaff00dee5379fe1eda75fbce51a76
pparadigm <priparadigm@gmail.com>
2013-07-24 16:50:55 -0500

    backup for end of workday, recognizes master key

--- whosin275.py
+++ whosin275.py
@@ -12,6 +12,7 @@
 # 1. Make it so RFIDKeyDict comes from a separate document
 # 2. Ability to have keys changed (added to system, deleted from system)
 # 3. Ask for port name on first run, have options to save preferences
+# 4. Create list and process of who is in the building
 #
 # ---------
 
@@ -20,31 +21,53 @@
 import serial
 
 
-# The 16-bit numbers appear when tags are scanned (they all have 0 at the
-# beginning, but Python doesn't like that to be there)
+# The 16-bit numbers originate from scanned tags
 RFIDKeyDict = {"06004FE1953D": "Joseph Livingdale",# 1 (see 'RFID Chart Info.txt')
                "06006F570B35": "Alice Varr",       # 2
                "06007266F7E5": "Grey Shultz",      # 3
                "0600727FF0FB": "MASTER"}           # 5
-count = 0
+
 listening = True
 
-
-# the port will need to be changed, depending on where you need to be listening
-# and your OS
-ser = serial.Serial("COM6", 9600)
-
-while listening:
-    count = count + 1
-    if count == 1:
-        serIn = ser.readline()[1:13]
-        if serIn in RFIDKeyDict:
-            print "Key recognized.\nWelcome, %s."%(RFIDKeyDict[serIn])
+def main():
+##    ser = serial.Serial(serialSetup(), 9600)
+    ser = serial.Serial("COM6", 9600)
+    count = 0
+    while listening:
+        count = count + 1
+        if count == 1:
+            serIn = ser.readline()[1:13]
         else:
-            print "Key not recognized."
-    else:
-        serIn = ser.readline()[2:14]
+            serIn = ser.readline()[2:14]
         if serIn in RFIDKeyDict:
-            print "Key recognized.\nWelcome, %s."%(RFIDKeyDict[serIn])
+            if RFIDKeyDict[serIn] == "MASTER":
+                masterKey()
+            else:
+                print "Key recognized.\nWelcome, %s."%(RFIDKeyDict[serIn])
         else:
-            print "Key not recognized."
+            print "Unrecognized key."
+
+
+def masterKey():
+    print "MASTER mode engaged."
+    # will come back to this later
+
+
+#in progress
+##def serialSetup():
+##    try:
+##        settings = open("settings.txt", "r")
+##        port = settings.readline()[17:]
+##        if port == "":
+##            raise IOError
+##    except IOError:
+##        settings = open("settings.txt", "w")
+##        port = input("What port is your RFID reader on?\n")
+##        save = input("Save this setting:\nTrue\nFalse")
+##        if save:
+##            settings.write("Port preference: ", port)
+##    settings.close()
+##    print port
+##    return port
+
+main()
