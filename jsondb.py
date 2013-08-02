4fa7253fafd59ac167431787386db42ada2e3953
Prime Paradigm <priparadigm@gmail.com>
2013-08-02 15:27:59 -0500

    PEP-complianted and made logging statements more grammatical.

--- jsondb.py
+++ jsondb.py
@@ -6,24 +6,26 @@ Documentation:
 __init__: takes datastore as an argument.  Datastore should be the path to the
 file where the database will be stored.
 
-addkey: adds a key to the system.  Takes key, user, and auth, and creates a new
-entry.  If upsert is set, an existing entry may be updated.  Teturns 0 on
+addKey: adds a key to the system.  Takes key, user, and auth, and creates a new
+entry.  If upsert is set, an existing entry may be updated.  Returns 0 on
 successful completion.
 
-delkey: removes a key from the system.  Takes key and returns status code (1 for
-error, 0 for successful completion)
+delKey: removes a key from the system.  Takes key and returns status code (1
+for error, 0 for successful completion)
 
-keylookup: looks up a key in the database.  Takes key and returns username, auth
-on success; otherwise, returns None.
+keyLookup: looks up a key in the database.  Takes key and returns username,
+auth on success; otherwise, returns None.
 
-namelist: takes no arguments, returns a list of users.
+nameList: takes no arguments, returns a list of users.
 
 _save: internal function called to save the database.  Called any time that an
 action modifies the database stored in RAM.
 
-_verifyt3: checks the t3 file before committing the backup by overwriting the old
-data file. Returns nothing on success, prints the db to the screen on error.
+_verifyt3: checks the t3 file before committing the backup by overwriting the
+old data file. Returns nothing on success, prints the db to the screen on error.
+
 '''
+
 import json
 import logging
 
@@ -31,65 +33,72 @@ import logging
 class db:
     def __init__(self, datastore):
         self.t3file = datastore
-        logging.debug("Loading 't3'->'t1'")
-        t3data=open(self.t3file)
-        self.db=json.load(t3data)
+        logging.debug("Loading 't3'->'t1'...")
+        t3data = open(self.t3file)
+        self.db = json.load(t3data)
         t3data.close()
+        logging.debug("Successful.")
 
-    def addkey(self, key, user, auth, upsert=False):
-        logging.info("Adding new key to system")
-        if key in self.db: #check if key exists
+    def addKey(self, key, user, auth, upsert = False):
+        logging.info("Attempting to add new key (%s) to system..."%(key))
+        # check if key exists
+        if key in self.db:
             if upsert:
-                self.db[key]={"user":user, "auth":auth}
+                self.db[key] = {"user": user, "auth": auth}
+                logging.info("Key added.")
                 return 0
             else:
-                logging.error("User '%s' already exists with key %s; (is upsert set?)",user,key) 
+                logging.error("User '%s' already exists with key %s. \
+(Is upsert set?)"%(user, key))
                 return 1
         else:
-            self.db[key]={"user":user, "auth": auth}
-            logging.info("Added user %s with key %s to database", user, key)
+            self.db[key]={"user": user, "auth": auth}
+            logging.info("Added user %s with key %s to database."%(user, key))
             self._save()
             return 0
 
-    def delkey(self, key):
-        logging.info("Removing key from system")
-        if key in self.db: #check if key exists
+    def delKey(self, key):
+        logging.info("Attempting to remove key (%s) from system..."%(key))
+        # check if key exists
+        if key in self.db:
             del self.db[key]
-            logging.info("Key id %s removed", key)
+            logging.info("Key removed.")
             self._save()
             return 0
         else:
-            logging.warning("Key does not exist")
+            logging.warning("Key cannot be removed; it does not exist.")
             return 1
         
-    def keylookup(self, key):
-        logging.info("Looking up key %s")
-        if key in self.db: #check if key exists
-            logging.info("Key found")
-            logging.debug("Returned data %s", self.db[key])
+    def keyLookup(self, key):
+        logging.info("Looking up key %s."%(key))
+        # check if key exists
+        if key in self.db:
+            logging.info("Key found.")
+            logging.debug("Returned data: %s"%(self.db[key]))
             return self.db[key]["user"], self.db[key]["auth"]
         else:
-            logging.warning("Key not found")
-            logging.debug("Key hash was %s", key)
+            logging.warning("Key not found.")
+            logging.debug("Key hash was %s."%(key))
             return None
 
-    def namelist(self):
-        names=[]
-        logging.info("Retrieving list of users")
+    def nameList(self):
+        names = []
+        logging.info("Retrieving list of users...")
         for key in self.db.keys():
             names.append(self.db[key]["user"])
-        logging.debug("%s", names)
+        logging.debug("%s"%(names))
         return names
 
     def _save(self):
-        logging.info("Syncing 't1'->'t3'")
-        t3data=open(self.t3file, 'w')
-        #sync t1data to t3data
-        json.dump(self.db, t3data, indent=2)
+        logging.info("Syncing 't1'->'t3'...")
+        t3data = open(self.t3file, 'w')
+        # sync t1data to t3data
+        json.dump(self.db, t3data, indent = 2)
         t3data.close()
+        logging.info("Successful sync.")
         
     def _confirmt3(self):
-        logging.debug("Confirming t3 integrity")
-        t3data=open(self.t3file)
-        self.dbtest=json.load(t3data)
+        logging.debug("Confirming t3 integrity...")
+        t3data = open(self.t3file)
+        self.dbtest = json.load(t3data)
         t3data.close()
