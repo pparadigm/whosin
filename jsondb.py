7ba7ef9190686eef8329b86d7962d7630ffbd04d
Michael Aldridge <maldridge@blackmac.(none)>
2013-08-01 21:26:40 -0500

    removed unneeded functions; renamed sync to save

--- jsondb.py
+++ jsondb.py
@@ -10,27 +10,30 @@ class db:
         #self.db{key: {"user":user, "auth": auth}} #load the data
         t3data.close()
 
-    def addkey(self, key, user, auth):
+    def addkey(self, key, user, auth, upsert=False):
         logging.info("Adding new key to system")
         if key in self.db: #check if key exists
-            logging.warning("User already exists with key {0}",key) 
-            return(1)
+            logging.warn("User already exists with key {0}",key) 
+            if upsert:
+                #add upsert functionality
+                pass
+            return 1
         else:
             self.db[key]={"user":user, "auth": auth}
             logging.info("Added user {0} with key {1} to database", user, key)
-            self._dbsync()
-            return(0)
+            self._save()
+            return 0
 
     def delkey(self, key):
         logging.info("Removing key from system")
         if key in self.db: #check if key exists
             del self.db[key]
             logging.info("Key id {0} removed", key)
-            self._dbsync()
-            return(0)
+            self._save()
+            return 0
         else:
-            logging.warning("Key does not exist")
-            return(1)
+            logging.warn("Key does not exist")
+            return 1
         
     def keylookup(self, key):
         logging.info("Looking up key {0}")
@@ -41,14 +44,11 @@ class db:
         else:
             logging.warning("Key not found")
             logging.debug("Key hash was {0}", key)
-            return(None)
-    def forcedbsave(self):
-        logging.info("DB sync was forced")
-        self._dbsync()
+            return None
 
-    def _dbsync(self):
-            logging.info("Syncing 't1'->'t3'")
-            t3data=open(self.t3file, 'w')
-            #sync t1data to t3data
-            json.dump(self.db, t3data, indent=2)
-            t3data.close()
+    def _save(self):
+        logging.info("Syncing 't1'->'t3'")
+        t3data=open(self.t3file, 'w')
+        #sync t1data to t3data
+        json.dump(self.db, t3data, indent=2)
+        t3data.close()
