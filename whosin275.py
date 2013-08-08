13c1916d7ecb5328005e53347d8c6797ecaaf197
Prime Paradigm <priparadigm@gmail.com>
2013-08-08 10:13:10 -0500

    improved database organization, added logging statements

--- whosin275.py
+++ whosin275.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # written in Python 2.7.5 by Prime Paradigm (@pparadigm on GitHub)
 # developed on a Win7 system
-# last updated: August 7, 2013 @ 1:28PM
+# last updated: August 8, 2013 @ 9:41AM
 
 # This program is meant to handle the door management process with the RFID
 # keys, and allow management of the system. Might track who is in the building.
@@ -37,28 +37,29 @@ listening = True
 
 def keyRetrieval(ID):
     logging.info("Retrieving key info from database...")
-    for index in range(len(keyDB)):
-        if ID in keyDB[index]:
-            for person in keyDB[index][ID]:
-                level = int(keyDB[index][ID][person])
-                logging.info("Information found.")
-                logging.debug("Key returned name of %s and access level \
+    try:
+        for person in keyDB[ID]:
+            level = int(keyDB[ID][person])
+            logging.info("Information found.")
+            logging.debug("Key returned name of %s and access level \
 %i."%(person, level))
-                return level, person
-    logging.info("No information found.")
-    return None, None
+            return level, person
+    except KeyError:
+        logging.info("No information found.")
+        return None, None
 
 
 def doorRetrieval():
-    # For now, door is hardcoded.
+    # For now, door is hardcoded. This will cause errors if the test database
+    # for doors isn't properly renamed and put into the main directory of this
+    # program.
     door = "Front Door IN"
     logging.info("Tag scanned at %s."%(door))
     # door will never not be in the database, as it is equivalent to the
     # scanner that the key ID is sent from.
     logging.info("Retrieving door info from database...")
-    for index in range(len(doorDB)):
-        if door in doorDB[index]:
-            level = doorDB[index][door]
+    if door in doorDB:
+        level = doorDB[door]
     logging.debug("Door lookup returned required access level of %i."%(level))
     return level
 
@@ -85,9 +86,11 @@ def access(scan):
 
 def portConfig():
     try:
+        logging.info("Looking for configuration...")
         setDoc = open("whosin.conf", "r")
         name, rate = setDoc.readline().split(" : ")
         setDoc.close()
+        logging.info("Configuration found.")
     except (ValueError, IOError):
         logging.warning("No configuration found, attempting to create one...")
         name = str(raw_input("System port descripter (case-sensitive): "))
@@ -99,7 +102,9 @@ def portConfig():
             setDoc = open("whosin.conf", "w")
             setDoc.write(settings)
             setDoc.close()
+            logging.info("Configuration saved.")
         else:
+            logging.info("Configuration was not saved.")
             print "Settings not saved."
     return name, rate
 
@@ -117,16 +122,16 @@ def startup():
         keyDB = keyDoc.readline()
     except IOError:
         keyDoc = open("keys.db", "w")
-        keyDoc.write("[{}]")
-        keyDB = "[{}]"
+        keyDoc.write("{}")
+        keyDB = "{}"
     keyDoc.close()
     try:
         doorDoc = open("doors.db", "r")
         doorDB = doorDoc.readline()
     except IOError:
         doorDoc = open("doors.db", "w")
-        doorDoc.write("[{}]")
-        doorDB = "[{}]"
+        doorDoc.write("{}")
+        doorDB = "{}"
     doorDoc.close()
     keyDB = json.loads(keyDB)
     doorDB = json.loads(doorDB)
